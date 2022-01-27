afacbfba95c6ae28b9da590d4563a8d06cb83164
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-27 14:26:56 +0100

    created Vue element

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('JS OK', Vue);
+/*
+Descrizione:
+Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+Deve essere possibile scorrere le immagini cliccando sulle freccette.
+I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+*/
+
+const root = new Vue({
+    el: '#root',
+    data: {
+
+    }
+})
